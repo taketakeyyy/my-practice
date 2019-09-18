ccb8ede65eafa36f251f5885e02c9d6779ca8694
taketakeyyy <taketakeyyy@gmail.com>
2019-09-18 19:58:19 +0900

    テスト駆動Pythonをすすめた

--- PythonTestingWithPytest/ch1/test_one.py
+++ PythonTestingWithPytest/ch1/test_one.py
@@ -1,2 +1,10 @@
 def test_passing():
     assert (1, 2, 3) == (1, 2, 3)
+    print("End!")
+
+"""
+$ pytest test_one.py
+
+# 詳細なコマンドが知りたいならば、
+$ pytest -v test_one.py
+"""
--- PythonTestingWithPytest/ch1/test_two.py
+++ PythonTestingWithPytest/ch1/test_two.py
@@ -1,2 +1,6 @@
 def test_falling():
     assert(1,2,3) == (3,2,1)
+
+def hoge():
+    """pytestの命名規則にあてはまらないので、テスト実行されない"""
+    assert 1 == 2
